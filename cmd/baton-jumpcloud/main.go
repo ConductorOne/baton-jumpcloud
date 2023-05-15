5b826959a17b687cef1eb3c7f1ba57beec860754
Justin Gallardo <justin.gallardo@conductorone.com>
2023-05-15 08:36:20 -0700

    Bump baton-sdk to v0.0.30 for provisioning support

--- cmd/baton-jumpcloud/main.go
+++ cmd/baton-jumpcloud/main.go
@@ -67,14 +67,6 @@ func run(ctx context.Context, cfg *config) error {
 
 	var opts []sdk.Option
 
-	if cfg.GrantPrincipalID != "" && cfg.GrantEntitlementID != "" && cfg.GrantPrincipalType != "" {
-		opts = append(opts, sdk.WithGrant(cfg.GrantEntitlementID, cfg.GrantPrincipalID, cfg.GrantPrincipalType))
-	}
-
-	if cfg.RevokeGrantID != "" {
-		opts = append(opts, sdk.WithRevoke(cfg.RevokeGrantID))
-	}
-
 	r, err := sdk.NewConnectorRunner(ctx, c, cfg.C1zPath, opts...)
 	if err != nil {
 		l.Error("error creating connector runner", zap.Error(err))
